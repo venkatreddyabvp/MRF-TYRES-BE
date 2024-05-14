4c9f6cbe3080abcb1615b5e097133cdd4f3c643f
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-05-14 16:05:37 +0530

    added the user

--- models/user-model.js
+++ models/user-model.js
@@ -14,6 +14,11 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ["owner", "worker"],
+    required: true,
+  },
 });
 
 const User = model("User", userSchema);
